98d07a952769dc3df71deabeea97d3d2be432797
Yh0N <133159832+Yh0N@users.noreply.github.com>
2026-05-17 20:30:25 -0500

    Increase total levels and update points for completion Aumenta los niveles totales y actualiza los puntos por completar.

--- game-project/src/Experience/World/LevelManager.js
+++ game-project/src/Experience/World/LevelManager.js
@@ -1,33 +1,43 @@
 export default class LevelManager {
     constructor(experience) {
         this.experience = experience;
-        this.currentLevel = 1;  // Inicias en el nivel 1
-        this.totalLevels = 2;   // Total de niveles 
+        this.currentLevel = 1;
+        this.totalLevels = 5;
+
+        this.pointsToComplete = {
+            1: 5,
+            2: 5,
+            3: 5,
+            4: 5,
+            5: 5
+        };
     }
 
     nextLevel() {
         if (this.currentLevel < this.totalLevels) {
             this.currentLevel++;
-    
             this.experience.world.clearCurrentScene();
             this.experience.world.loadLevel(this.currentLevel);
-    
-            // Espera breve para que el nivel se cargue y luego reubicar al robot
-            setTimeout(() => {
-                this.experience.world.resetRobotPosition({ x: -17, y: 1.5, z: -67 }) //  Ajusta esta coordenada según el mundo nuevo
-            }, 1000)
         }
     }
-    
 
     resetLevel() {
         this.currentLevel = 1;
         this.experience.world.loadLevel(this.currentLevel);
     }
 
-
     getCurrentLevelTargetPoints() {
-        return this.pointsToComplete?.[this.currentLevel] || 2;
+        return this.pointsToComplete?.[this.currentLevel] || 5;
     }
-    
+
+    getEnemiesForLevel(level) {
+        const enemies = {
+            1: 4,
+            2: 8,
+            3: 10,
+            4: 17,
+            5: 18
+        }
+        return enemies[level] || 1
+    }
 }
